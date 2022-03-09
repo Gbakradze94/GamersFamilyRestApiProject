290199403f40a0371e6708dfa328621096b34226
Giorgi Bakradze <Bakradzegeorge17@gmail.com>
2022-03-09 15:10:12 +0400

    Fix: @Repository annotation not needed when extending Repository interface

--- src/main/java/com/gamersfamily/gamersfamily/repository/GameRepository.java
+++ src/main/java/com/gamersfamily/gamersfamily/repository/GameRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
+
 public interface GameRepository extends PagingAndSortingRepository<Game,Long> {
 
     List<Game> findAll();
