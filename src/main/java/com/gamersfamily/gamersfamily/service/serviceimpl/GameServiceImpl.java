776efa1a52aad6b3567ada9bfaf7dd1161f7b238
Giorgi Bakradze <Bakradzegeorge17@gmail.com>
2022-03-09 16:38:31 +0400

    Fix: Replace @Autowired with constructor injection

---
+++ src/main/java/com/gamersfamily/gamersfamily/service/serviceimpl/GameServiceImpl.java
@@ -0,0 +1,47 @@
+package com.gamersfamily.gamersfamily.service.serviceimpl;
+
+
+import com.gamersfamily.gamersfamily.dto.GameDto;
+import com.gamersfamily.gamersfamily.model.Game;
+import com.gamersfamily.gamersfamily.repository.GameRepository;
+import com.gamersfamily.gamersfamily.service.GameService;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GameServiceImpl implements GameService {
+
+    private GameRepository gameRepository;
+
+    private ModelMapper modelMapper;
+
+    public GameServiceImpl(GameRepository gameRepository, ModelMapper modelMapper){
+        this.gameRepository = gameRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public List<GameDto> getAllGames() {
+        return gameRepository.findAll()
+                .stream().map(game -> modelMapper.map(game,GameDto.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<GameDto> getGamesByPage(Integer pageNumber, Integer pageSize) {
+        Pageable pages = PageRequest.of(pageNumber,pageSize);
+        return gameRepository.findAll(pages)
+                .getContent()
+                .stream()
+                .map(game -> modelMapper.map(game,GameDto.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Game saveGame(GameDto gameDto) {
+        return gameRepository.save(modelMapper.map(gameDto,Game.class));
+    }
+}
